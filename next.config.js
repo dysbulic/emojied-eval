8ab2bdc38b4e70cca6d1ec0e5e9e4a793ab06f1c
dysbulic <dys@dhappy.org>
2024-01-11 17:48:06 -0500

    addin’ WAGMI & Supabase packages 🏔

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
