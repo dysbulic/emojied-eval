8ce1b24cf41da8b3de80f56936ce9a2b70661fa7
dysbulic <dys@dhappy.org>
2024-01-17 02:36:25 -0500

    puttin’ the logo on the homepage 😡

--- vite-emojied-eval/src/components/Home/index.tsx
+++ vite-emojied-eval/src/components/Home/index.tsx
@@ -2,7 +2,10 @@ import LoginButton from "../LoginButton"
 import tyl from './index.module.css'
 
 export const Home = () => (
-  <LoginButton className={tyl.main}/>
+  <section id="home">
+    <img src="/logo.svg" alt="Serial Mobbing"/>
+    <LoginButton className={tyl.main}/>
+  </section>
 )
 
 export default Home
