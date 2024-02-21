aecd6b5b0320e897b95d5975dd5747817384b1f3
dysbulic <dys@dhappy.org>
2024-02-21 02:45:03 -0500

    deploying to Pages

---
+++ assets/SuspendedReactor-TyzFlL2M.js
@@ -0,0 +1,1 @@
+import{j as r,r as t}from"./index-a8h7Vdce.js";import{Reactor as o}from"./index-sxHoz_vF.js";import"./utils-RhknTK99.js";import"./index-YDASq1yd.js";import"./useQuery-0fXINSu8.js";import"./index-AsRL7gF_.js";const n=()=>r.jsx(t.Suspense,{fallback:r.jsx("div",{children:"Loading…"}),children:r.jsx(o,{})});export{n as SuspendedReactor,n as default};
