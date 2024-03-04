a6ecd7db6d70007376328eed6857168e51898503
dysbulic <dys@dhappy.org>
2024-03-03 23:37:41 -0500

    deploying to Pages

---
+++ assets/Configuration-gxy4H8aU.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-65daIVkk.js";const t=()=>o.jsx("h1",{children:"¡Configure!"});export{t as Configuration,t as default};
--- assets/Configuration-lPNDPQvT.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-qdUFMJ2-.js";const t=()=>o.jsx("h1",{children:"¡Configure!"});export{t as Configuration,t as default};
